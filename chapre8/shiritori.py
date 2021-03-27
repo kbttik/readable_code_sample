ef6cb45d5c4b2c8b128692f266a334fab3a1fc76
Kaito Hasegawa <kaitohasegawa@KaitonoMac-mini.local>
2021-03-28 01:34:49 +0900

    ひとりしりとりのベース

---
+++ chapre8/shiritori.py
@@ -0,0 +1,17 @@
+def shiritori(word, next_head):
+    # TODO: 実装してください
+    return True, 'aaa'
+
+def main():
+    word = ''
+    next_head = ''
+    while(True):
+        word = input().strip()
+        jadge, next_head = shiritori(word, next_head)
+        if not jadge or word == "quit":
+            print("NG")
+            break
+        print("OK")
+
+if __name__ == "__main__":
+    main()
