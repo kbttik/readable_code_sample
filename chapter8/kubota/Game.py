8db86c55d4e231d5fa385788c123a958ab2f314a
kbttik <taiki.j.mac@gmail.com>
2021-04-13 01:26:31 +0900

    実行部分をclassから出す

--- chapter8/kubota/Game.py
+++ chapter8/kubota/Game.py
@@ -2,7 +2,7 @@ class Game:
     """しりとりゲームクラス
     """
 
-    def __init__(self, min_word_len:int):
+    def __init__(self, min_word_len:int, max_count:int):
         """ゲームの負け条件を入れる
 
         Args:
@@ -11,6 +11,9 @@ class Game:
         self.min_word_len = min_word_len
         self.words = []
         self.last_key = ''
+        self.now_count = 0
+        self.max_count = max_count
+        self.is_lose = False
 
     def is_lose_by_len(self, word:str) -> bool:
         """ワードの長さがゲームの基準を満たしていなくて負けているか
@@ -23,6 +26,18 @@ class Game:
             bool: 負けTrue/満たしているFalseの論理値
         """
         return len(word) < self.min_word_len
+    
+    def count_under(self, word:str):
+        """X文字列以下のcountを足していく
+
+        Args:
+            word (str): [description]
+        """
+        if len(word) >= self.min_word_len:
+            self.now_count = 0
+        else
+            self.now_count += 1
+
     
     def is_lose_by_tail(self, word:str) -> bool:
         """ワードの最後が「ん」で負けているか
@@ -61,6 +76,24 @@ class Game:
         """
         return word in self.words
 
+
+    def judge_lose(self, word:str) -> bool:
+        """ステータスから状態を評価する
+
+        Args:
+            word (str): [description]
+        """
+        self.is_lose = any([self.is_lose_by_len(word), self.is_lose_by_tail(word), self.is_lose_by_match(word), self.is_lose_by_already(word)])
+
+    def set_word(self, word:str):
+        """wordリストに追加と、次の先頭文字の設定
+
+        Args:
+            word (str): [description]
+        """
+        self.words.append(word)
+        self.last_key = word[-1]
+
     def judge_is_lose(self, word:str) -> bool:
         """全部の負けを評価する
 
@@ -87,7 +120,17 @@ class Game:
 
 def main():
     game = Game(5)
-    game.exec_game()
+    #game.exec_game()
+    while(True):
+        print('ワードを入力')
+        word = input().strip()
+        if game.judge_lose(word):
+                print('NG')
+                break
+            print('OK')
+        # 最後の文字を更新
+        # 使ったワードリストに追加
+        game.set_word(word)
 
 if __name__ == '__main__':
     main()
