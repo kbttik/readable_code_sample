cb2457b2c532032214ea52381f60160a6f6b91f8
kbttik <taiki.j.mac@gmail.com>
2021-04-13 01:27:06 +0900

    しりとりのテスト、標準入力のテストも可能

---
+++ chapter8/test_shiritori.py
@@ -0,0 +1,41 @@
+
+import contextlib
+import io
+import sys
+from kaito.shiritori import shiritori as kaito_shiritori
+
+def test_しりとり成功(monkeypatch):
+    word = ''
+    next_head = ''
+    words = []
+    for word in ['りんごじゅーす', 'すりがらす', 'すずめのなみだ', 'だちょう', 'うんこ']:
+        monkeypatch.setattr('sys.stdin', io.StringIO(word))
+        print('ワードを入力:')
+        word = input().strip()
+        jadge, next_head = kaito_shiritori(word, next_head, words)
+        if not jadge or word == 'quit':
+            print('NG')
+            assert False
+            break
+        print('OK')
+        words.append(word)
+
+
+def test_しりとり失敗_末尾と先頭の不一致(monkeypatch):
+    word = ''
+    next_head = ''
+    words = []
+    for word in ['りんご', 'すりがらす']:
+        monkeypatch.setattr('sys.stdin', io.StringIO(word))
+        print('ワードを入力:')
+        word = input().strip()
+        jadge, next_head = kaito_shiritori(word, next_head, words)
+        if not jadge or word == 'quit':
+            print('NG')
+            assert True
+            break
+        print('OK')
+        words.append(word)
+    
+    assert not jadge
+
