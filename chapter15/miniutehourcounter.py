5d9788b489d781db85d1fda56c6513da2a28e5db
MuramasaExs <majimajayo@yahoo.co.jp>
2021-07-24 21:27:24 +0900

    countの実装

--- chapter15/miniutehourcounter.py
+++ chapter15/miniutehourcounter.py
@@ -15,8 +15,32 @@ class MinuteHourCounter:
 
     #直近60秒間の累積のカウントを返す
     def minute_count(self):
-         pass
+        ret_count = 0
+        now_secs = datetime.now().timestamp()
+        for i in range(len(self.events)):
+            if (now_secs - self.events[-(i+1)].time)  > 60:
+                break
+            ret_count = ret_count + self.events[-(i+1)].count
+
+
+        return ret_count
 
     #直近3600秒間の累積カウントを返す
     def hour_count(self):
-         pass
+        ret_count = 0
+        now_secs = datetime.now().timestamp()
+        for i in range(len(self.events)):
+            if (now_secs - self.events[-(i+1)].time)  > 3600:
+                break
+            ret_count = ret_count + self.events[-(i+1)].count
+
+
+        return ret_count
+
+mh = MinuteHourCounter()
+mh.add(2)
+mh.add(3)
+mh.add(6)
+mh.add(7)
+mh.add(10)
+mh.minute_count()
